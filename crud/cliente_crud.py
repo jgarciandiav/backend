a3aa59bf9cc80490aa94841cdf02ae8dbaab2638
juan <jgarciandia@gmail.com>
2026-02-04 17:50:39 -0500

    cambio para actualizar clientes

--- crud/cliente_crud.py
+++ crud/cliente_crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from models import Cliente
+from models import Cliente, Factura
 from schemas import ClienteCreate
 
 from sqlalchemy import func
@@ -27,7 +27,31 @@ def create(db: Session, obj: ClienteCreate):
     return db_obj
 
 def update(db: Session, id: int, obj: ClienteCreate):
+    # Get the old customer data before updating
+    old_cliente = get_by_id(db, id)
+    if not old_cliente:
+        return None
+    
+    old_name = old_cliente.name
+    old_address = old_cliente.address
+    
+    # Update the customer
     db.query(Cliente).filter(Cliente.id == id).update(obj.dict())
+    db.commit()
+    
+    # Update all invoices (facturas) that have this customer
+    # Update customer name if it changed
+    if old_name != obj.name:
+        db.query(Factura).filter(Factura.customer == old_name).update({
+            "customer": obj.name
+        })
+    
+    # Update address for all invoices with this customer (using new name)
+    # This handles both cases: name changed or address changed
+    db.query(Factura).filter(Factura.customer == obj.name).update({
+        "address": obj.address
+    })
+    
     db.commit()
     return get_by_id(db, id)
 
